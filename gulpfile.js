5739fb7192a81eae077935d18b7fab2175e7e85e
Marshall Sumwalt <MarshallSumwalt@gmail.com>
2015-01-09 16:27:37 -0600

    Started updating gulp and package.json to have everything build properly. @myasonik helped me out a lot here and I used his Front-end_Boilerplate as an example

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
-var gulp = require('gulp');
+var del     = require('del'),
+    gulp    = require('gulp'),
+    // jade
+    jade    = require('gulp-jade'),
+    // js
+    jshint  = require('gulp-jshint'),
+    uglify  = require('gulp-uglify');
 
-gulp.task('default', function() {
-
+gulp.task('default', ['clean'], function() {
+    gulp.start('move')
 });
+
+
+gulp.task('move', function() {
+        // Resources
+        gulp.src('src/resources/*')
+            .pipe(gulp.dest("dist/resources"))
+});
+
+gulp.task('clean', del.bind(null, ['dist/**/*', '!.*']));
